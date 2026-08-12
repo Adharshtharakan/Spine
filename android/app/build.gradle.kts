71a7003422fab8db2e1243be6e08e14194db9eed
angith-anmesrisha <angithvshaji3@gmail.com>
2026-08-13 02:07:08 +0530

    First Build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         applicationId = "com.spineapp.spine"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
